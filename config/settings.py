8ed1fa31c4091d2458e2d77aac13687f6f6dd125
Thierry de Ridder Martins Varela <varelathierry@gmail.com>
2026-09-05 23:43:10 -0300

    feat: adding page not found

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "127.0.0.1,localhost",
+).split(",")
 
 
 # Application definition
