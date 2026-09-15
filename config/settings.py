4f775be9069ced5e661646d3add6ce0de8c54613
Thierry de Ridder Martins Varela <varelathierry@gmail.com>
2026-09-15 11:55:51 -0300

    fix: separate dependencies and configure static storage by environment

--- config/settings.py
+++ config/settings.py
@@ -169,7 +169,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": (
+            "django.contrib.staticfiles.storage.StaticFilesStorage"
+            if DEBUG
+            else "whitenoise.storage.CompressedManifestStaticFilesStorage"
+        ),
     },
 }
 
